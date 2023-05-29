26bc3d977caff36fa0e0bac336407ddede6124b1
Yoav Schwartz <yoavsc@mta.ac.il>
2023-05-29 16:57:53 +0300

    Add projects

---
+++ B23 Ex04 Yoav 208786830 Barak 313170730/Ex04.Menus.Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex04.Menus.Test
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
