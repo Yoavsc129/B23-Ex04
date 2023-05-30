2cb22fc31c05ed7f0850ad434837c0c81357878e
barakg43 <93736137+barakg43@users.noreply.github.com>
2023-05-30 21:41:03 +0300

    start working on the interface

---
+++ B23 Ex04 Yoav 208786830 Barak 313170730/Ex04.Menus. Interfaces/MenuItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex04.Menus.Interfaces
+{
+    class MenuItem
+    {
+        private readonly List<MenuItem> r_CurrentMenuItemsList;
+
+        public MenuItem()
+        {
+            r_CurrentMenuItemsList = new List<MenuItem>();
+        }
+
+        public bool MenuItemIsLeaf()
+        {
+            return r_CurrentMenuItemsList.Count == 0;
+        }
+
+        public void AddMenuItemToList(IItemAction menuItemAction)
+    }
+}
